695b0b0f0601e3d36367855826cb856b744b8435
hermessil <Silas.Hermes02@gmail.com>
2024-08-06 19:49:54 +0200

    setup class LuckyDice

---
+++ LuckyDice.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+class LuckyDice:
+    def __init__(self):
+        amount_dice: int = 3
+        symbol_to_dict = ["officer", "car", "train", "ring", "bulb", "faucet"]
+
+    def roll_dice():
+        return np.random.randint(1, 6)
